26001d284d82118c8e2db68773f83cd668f10454
Benjamin Breton <bbenjaminbreton@gmail.com>
2021-03-22 14:43:30 +0100

    🔴⚙️ write delete performance tests : fails

--- CarrotsTests/CarrotsTests.swift
+++ CarrotsTests/CarrotsTests.swift
@@ -132,10 +132,21 @@ class CarrotsTests: XCTestCase {
         addAthletic("Ben", to: game)
         addSport("Marche", to: game)
         game.addPerformance(sport: game.sports[0], athletics: game.athletics, value: [10])
-        guard let pot = game.commonPot else {
-            XCTFail()
-            return
-        }
+        let pot = getPot(game: game)
+        XCTAssert(pot.points == 10)
+        XCTAssert(game.performances.count == 1)
+    }
+    
+    func testGivenPerformancesExistWhenAskToDeleteOneOfThemThenPerformanceIsDeleted() {
+        let coreDataStack = getCoreDataStack()
+        let game = Game.initGame(coreDataStack: coreDataStack)
+        addAthletic("Ben", to: game)
+        addSport("Marche", to: game)
+        addSport("Rameur", to: game)
+        game.addPerformance(sport: game.sports[0], athletics: game.athletics, value: [10])
+        game.addPerformance(sport: game.sports[1], athletics: game.athletics, value: [100])
+        game.deletePerformance(performance: game.performances[0])
+        let pot = getPot(game: game)
         XCTAssert(pot.points == 10)
         XCTAssert(game.performances.count == 1)
     }
@@ -158,5 +169,19 @@ class CarrotsTests: XCTestCase {
             return FakeCoreDataStack()
         }
         return coreDataStack
+    }
+    func getPot(of owner: Athletic? = nil, game: Game) -> Pot {
+        if let owner = owner {
+            guard let pot = owner.pot else {
+                XCTFail()
+                return Pot()
+            }
+            return pot
+        }
+        guard let pot = game.commonPot else {
+            XCTFail()
+            return Pot()
+        }
+        return pot
     }
 }
